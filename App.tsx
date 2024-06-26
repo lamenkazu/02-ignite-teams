ef4b0aa7d51fac325f2687b0a1fb4f551a490941
Lamenkazu <333.erick@gmail.com>
2024-06-26 11:31:04 -0300

    ✨ Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { Groups } from "@/screens/Groups";
 import theme from "@/theme";
+
 import { ThemeProvider } from "styled-components/native";
 
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {!fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
