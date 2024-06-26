222c0fabb17bf9b153061a6d549994c39f9b62f7
Lamenkazu <333.erick@gmail.com>
2024-06-26 15:42:44 -0300

    ✨ Utilizando o CSS Helper do Styled Components + Refatorando a estilziação

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -1,6 +1,7 @@
 import UsersThree from "phosphor-react-native/src/icons/UsersThree";
 
 import { TouchableOpacity } from "react-native";
+import { css } from "styled-components";
 import styled from "styled-components/native";
 
 export const Container = styled(TouchableOpacity)`
@@ -16,9 +17,11 @@ export const Container = styled(TouchableOpacity)`
 `;
 
 export const Title = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  color: ${({ theme }) => theme.COLORS.GRAY_200};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+  `}
 `;
 
 export const Icon = styled(UsersThree).attrs(({ theme }) => ({
