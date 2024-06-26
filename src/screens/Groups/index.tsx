dbdf778180d886441543d5a062115130be2d062e
Lamenkazu <333.erick@gmail.com>
2024-06-26 14:38:44 -0300

    ✨ Componente Empty List

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,13 +3,27 @@ import { Container } from "./styles";
 import { Highlight } from "@/components/Highlight";
 import { GroupCard } from "@/components/GroupCard";
 
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { EmptyList } from "@/components/EmptyList";
+
 export const Groups = () => {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Clashzada" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <EmptyList message="Que tal cadastrar a primeira turma?" />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }} // Centraliza na tela caso groups esteja vazio
+      />
     </Container>
   );
 };
