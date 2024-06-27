13e1bbe952d2c8d91b37ef713304874c21c43efb
Lamenkazu <333.erick@gmail.com>
2024-06-27 17:15:39 -0300

    ✨ Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -7,9 +7,16 @@ import { useState } from "react";
 import { FlatList } from "react-native";
 import { EmptyList } from "@/components/EmptyList";
 import { Button } from "@/components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export const Groups = () => {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const { navigate } = useNavigation();
+
+  const handleNewGroup = () => {
+    navigate("new");
+  };
 
   return (
     <Container>
@@ -26,7 +33,7 @@ export const Groups = () => {
         contentContainerStyle={groups.length === 0 && { flex: 1 }} // Centraliza na tela caso groups esteja vazio
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 };
