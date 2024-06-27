1e8306f86fd139d644235c3f0d128c6dbe3585de
Lamenkazu <333.erick@gmail.com>
2024-06-27 14:19:06 -0300

    ⚙️👨‍💻 Instalando Stack Navigator do React Navigation

--- App.tsx
+++ App.tsx
@@ -1,4 +1,9 @@
+import { StatusBar } from "react-native";
+
+import { Loading } from "@/components/Loading";
 import { Groups } from "@/screens/Groups";
+import { NewGroup } from "@/screens/NewGroup";
+import { Players } from "@/screens/Players";
 import theme from "@/theme";
 
 import { ThemeProvider } from "styled-components/native";
@@ -8,10 +13,6 @@ import {
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
-import { Loading } from "@/components/Loading";
-import { StatusBar } from "react-native";
-import { NewGroup } from "@/screens/NewGroup";
-import { Players } from "@/screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
